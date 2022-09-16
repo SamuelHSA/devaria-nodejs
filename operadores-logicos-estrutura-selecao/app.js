29afc229da022f2bf35028146d903174b090da19
SamuelHSA <111927634+SamuelHSA@users.noreply.github.com>
2022-09-16 19:52:30 -0300

    operadores-logicos-estrutura-selecao

---
+++ operadores-logicos-estrutura-selecao/app.js
@@ -0,0 +1,7 @@
+const readLine = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+console.log('Esse programa vai checar se voce e maior de 18 anos e tem habilitacao para saber se voce pode entrar no kart');
+console.log('alem das suas verificacoes, precisamos verificar se voce esta na lista de presenca do horario');
