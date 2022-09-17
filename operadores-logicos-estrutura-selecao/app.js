2267556482f75db479c17ff1e5bc98824b99d68d
SamuelHSA <111927634+SamuelHSA@users.noreply.github.com>
2022-09-17 11:27:37 -0300

    Operadores e estrutura de seleção

--- operadores-logicos-estrutura-selecao/app.js
+++ operadores-logicos-estrutura-selecao/app.js
@@ -5,3 +5,28 @@ const readLine = require('readline').createInterface({
 
 console.log('Esse programa vai checar se voce e maior de 18 anos e tem habilitacao para saber se voce pode entrar no kart');
 console.log('alem das suas verificacoes, precisamos verificar se voce esta na lista de presenca do horario');
+
+readLine.question('Qual o ano do seu nascimento?', ano => {
+    if(ano > 2004){
+        console.log('Voce nao tem 18 anos');
+    }else{
+        readLine.question("Voce tem habilitacao? (Sim/Nao)", temHabilitacao => {
+            if(!(temHabilitacao.toUpperCase() === "SIM")){
+                console.log('Voce nao tem habilitacao para entrar no kart');
+            }else{
+                readLine.question("Qual seu nome?", nome =>{
+                    switch(nome){
+                        case 'Samuel' :
+                            console.log('Bem vindo ao Kart Samuel');
+                            break;
+                        case 'Rafael' :
+                            console.log('Bem vindo ao Kart Rafael');
+                            break;
+                        default:
+                                console.log('Seu nome nao foi identificado na lista de presenca');
+                    }
+                });
+            }
+        })
+    }
+})
